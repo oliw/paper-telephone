fd5a4af2f669f7f55de8b64a58f9c6709ffbbba4
Oli W <oli@oliverwilkie.com>
2020-03-30 21:08:47 -0700

    Updates

---
+++ precache-manifest.d4e380b9439fa8d65fa48414373a3812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b826e3ad4ec0f44948a0238eda9076c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2df45e2f57b96533f4e0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "17cf6b97b45ad8e3584b",
+    "url": "/static/js/2.859aa264.chunk.js"
+  },
+  {
+    "revision": "581070db7478fba8a52f7e9e4c335e11",
+    "url": "/static/js/2.859aa264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df45e2f57b96533f4e0",
+    "url": "/static/js/main.aa0df22d.chunk.js"
+  },
+  {
+    "revision": "a924c9d48ca39a16fae5",
+    "url": "/static/js/runtime-main.d1cd7c3b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paper-telephone/precache-manifest.dd6e81566091b96824af71c3997d761b.js"
+  "/precache-manifest.d4e380b9439fa8d65fa48414373a3812.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paper-telephone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
