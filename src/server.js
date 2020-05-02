3e66b62cb8e7e8e6f93a9f2668f9592ae30e9916
Oli W <oli@oliverwilkie.com>
2020-05-01 20:00:36 -0700

    Add Mongo

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 // src/server.js
-const Server = require("boardgame.io/server").Server;
+const { Server, Mongo } = require("boardgame.io/server");
 const TelephoneGame = require("./model/telephone/game").Game;
 const SaladbowlGame = require("./model/saladbowl/game").Game;
-const server = Server({ games: [SaladbowlGame, TelephoneGame] });
+const server = Server({
+  games: [SaladbowlGame, TelephoneGame],
+  db: new Mongo({
+    url: process.env.MONGODB_URI,
+    dbname: "bgio",
+  }),
+});
 
 const port = process.env.PORT || 8000;
 
